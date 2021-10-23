ecb87c8d43e4b2b1642cb0519d67b8c385a6c39a
Jordan A Holt <jordanh316@gmail.com>
2021-10-23 06:35:54 -0500

    Fix Authenitcation Issue

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://adminHolt:<password>@cluster0.xnjco.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://adminHolt:Jayhawk1988@cluster0.xnjco.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 let Person;
